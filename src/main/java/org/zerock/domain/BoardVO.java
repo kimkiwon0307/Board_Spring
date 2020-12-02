2ea5097bdabb80cf415883872be6bad09381027f
kkwkkj@naver.com <kkwkkj@naver.com>
2020-12-02 14:22:28 +0900

    영속계층

--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -4,16 +4,13 @@ import java.util.Date;
 
 import lombok.Data;
 
-@Data
+@Data //Lombok을 이용해서 생성자와 getter/setter, toString() 등을 만들어냄
 public class BoardVO {
-
-	private Long bno;
 	
+	private Long bno;
 	private String title;
 	private String content;
 	private String writer;
 	private Date regdate;
-	
 	private Date updateDate;
-	
 }
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -24,39 +24,57 @@ public class BoardMapperTests {
 		mapper.getList().forEach(board -> log.info(board));
 
 	}
+	
 	@Test
 	public void testInsert() {
-
+		
 		BoardVO board = new BoardVO();
 		board.setTitle("새로 작성하는 글");
-		board.setContent("새로 작성하는 내용");
+		board.setContent("새로작성하는 내용");
 		board.setWriter("newbie");
+		
 		mapper.insert(board);
-		log.info(board); // Lombok이 만들어주는 toString()을 이용해서 bno멤버변수(인스턴스변수)의 값을 알아보기위함
+		
+		log.info(board);
 	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로작성하는 내용 select key");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
 	@Test
 	public void testRead() {
+		
 		BoardVO board = mapper.read(5L);
+		
 		log.info(board);
 	}
 	
 	@Test
 	public void testDelete() {
-		log.info(mapper.delete(5L));
+		log.info("DELETE COUNT: " + mapper.delete(3L));
 	}
-	
 	
 	@Test
 	public void testUpdate() {
-		
 		BoardVO board = new BoardVO();
-		board.setBno(3L);
-		board.setTitle("수정된제목");
-		board.setContent("수정된내용");
-		board.setWriter("수정된회원");
+		
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user000");
 		
 		int count = mapper.update(board);
-		
-		log.info(count);
+		log.info("UPDATE COUNT: " + count);
 	}
+	
 }
