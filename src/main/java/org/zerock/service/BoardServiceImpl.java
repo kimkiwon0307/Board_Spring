5eb1d6f7dc9c1954b30692d69a873d59efe4fe81
kkwkkj@naver.com <kkwkkj@naver.com>
2020-12-03 14:31:52 +0900

    service, controller

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,51 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+
+
+@Service  // 계층 구조상 주로 비즈니스 영역을 담당하는 객체임을 표시
+@AllArgsConstructor // 모든 파라미터를 이용하는 생성자를 만든다.
+public class BoardServiceImpl  implements BoardService{
+
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return mapper.update(board) ==1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return mapper.getList();
+	}
+	
+	
+
+}
