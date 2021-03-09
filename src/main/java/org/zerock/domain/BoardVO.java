eaad3a66a147feba5160a5f2e32dab9bee84b588
kkwkkj@naver.com <kkwkkj@naver.com>
2021-03-09 13:39:59 +0900

    영속계층의 CRUD 구현

--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 import lombok.Data;
 
-@Data //Lombok을 이용해서 생성자와 getter/setter, toString() 등을 만들어냄
+@Data  // Lombok을 이용해서 생성자와 getter/setter, toString()등을 만들기 위해 @Data어노테이션을 사용한다.
 public class BoardVO {
 	
 	private Long bno;
